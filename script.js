2a992137e8230b9540e7c7de491d4d0c77e1376a
Fernando Gauterio <fernando.gauterio@kunden.com.br>
2023-11-03 17:31:21 -0300

    iniciado a ativade 02

--- script.js
+++ script.js
@@ -46,3 +46,70 @@ function SumDays(){
     ResultInput.value = parseInt(TotalDaysYear) + parseInt(TotalDaysMonth) + parseInt(TotalDay) + " Dias";
 
  }
+
+ //-------------------------------------------
+
+const input02 = document.querySelectorAll(".input02");
+const result02 = document.querySelector("#resultado02");
+const btn02 = document.querySelector("#btn02")
+
+btn02.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    if (input02[0].value < 0) {
+        alert("Nota 01 não pode ser negativa!");
+        return;
+    }  
+    else if(input02[1].value < 0){
+        alert("Nota 02 não pode ser negativa!");
+        return;
+    }
+    else if(input02[2].value < 0){
+        alert("Nota 03 não pode ser negativa!");
+        return;
+    }
+    else if(input02[3].value < 0){
+        alert("Nota 04 não pode ser negativa!");
+        return;
+    }
+    MediaNotas();
+})
+
+function MediaNotas() {
+    let Nota01 = input02[0].value;
+    let Nota02 = input02[1].value;
+    let Nota03 = input02[2].value;
+    let Nota04 = input02[3].value;
+    let MaiorNota;
+    let Media;
+
+
+    if(Nota01 === ""){
+        alert("Nota 01 em branco, verifique!")
+        return;
+    }
+    else if(Nota02 === ""){
+        alert("Nota 02 em branco, verifique!")
+        return;
+    }
+    else if(Nota03 === ""){
+        alert("Nota 03 em branco, verifique!")
+        return;
+        
+    }
+    else if(Nota04 === ""){
+        alert("Nota 04 em branco, verifique!")
+        return;     
+    }
+
+    if(Nota03 > Nota04){
+        MaiorNota = Nota03;
+    }
+    else{
+        MaiorNota = Nota04; 
+    }
+
+    Media = (parseInt(Nota01) + parseInt(Nota02) + parseInt(MaiorNota)) / 3
+
+    result02.value = Media
+}
